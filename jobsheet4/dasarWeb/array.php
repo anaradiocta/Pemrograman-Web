10782e4a3b011b981854613f24bb5ecd32592907
anaradiocta <nelaocta12@gmail.com>
2024-03-10 21:07:08 +0700

    Jobsheet PHP-04

--- jobsheet4/dasarWeb/array.php
+++ jobsheet4/dasarWeb/array.php
@@ -1,3 +1,4 @@
+<?php
 $nilaiSiswa = [85, 92, 78, 64, 90, 55, 88, 79, 70, 96];
 
 $nilaiLulus = [];
@@ -8,7 +9,8 @@ foreach ($nilaiSiswa as $nilai){
     }
 }
 
-echo "Daftar  nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+echo "Daftar nilai siswa: " . implode(', ', $nilaiSiswa) . "<br>";
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus) . "<br>";
 
 $daftarKaryawan = [
     ['Alice', 7],
@@ -26,4 +28,49 @@ foreach ($daftarKaryawan as $karyawan){
     }
 }
 
-echo "Daftar $karyawan"
+echo "<br>Daftar karyawan: ";
+foreach ($daftarKaryawan as $karyawan) {
+    echo $karyawan[0] . " (" . $karyawan[1] . "), ";
+}
+echo "<br>Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: <br><br>";
+echo implode(', ', $karyawanPengalamanLimaTahun);
+
+$daftarNilai =[
+    'Matematika' => [
+        ['Alice', 85],
+        ['Bob', 92],
+        ['Charlie', 78],
+    ],
+    'Fisika' => [
+        ['Alice', 90],
+        ['Bob', 88],
+        ['Charlie', 75],
+    ],
+    'Kimia' => [
+        ['Alice', 92],
+        ['Bob', 80],
+        ['Charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+$mataKuliah1 = 'Matematika';
+$mataKuliah2 = 'Kimia';
+
+echo "<br>Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
+
+foreach ($daftarNilai[$mataKuliah] as $nilai){
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
+echo "<br>Daftar nilai mahasiswa dalam mata kuliah $mataKuliah1: <br>";
+
+foreach ($daftarNilai[$mataKuliah1] as $nilai){
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
+
+echo "<br>Daftar nilai mahasiswa dalam mata kuliah $mataKuliah2: <br>";
+
+foreach ($daftarNilai[$mataKuliah2] as $nilai){
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
+?>
--- jobsheet4/tipe_data.php
+++ jobsheet4/tipe_data.php
@@ -6,10 +6,10 @@ $d = $b + (10 * 5);
 $e = $d - $c;
 
 echo "Variabel a: {$a} <br>";
-echo "Variabel b: {$a} <br>";
-echo "Variabel c: {$a} <br>";
-echo "Variabel d: {$a} <br>";
-echo "Variabel e: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Variabel c: {$c} <br>";
+echo "Variabel d: {$d} <br>";
+echo "Variabel e: {$e} <br><br>";
 
 var_dump($e);
 
